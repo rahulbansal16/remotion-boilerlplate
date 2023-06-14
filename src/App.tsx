65abeb6fdda8dfb1a6c2c0ee44d404e22109196f
Rahul Bansal <bansal.rahul14@gmail.com>
2023-06-14 10:52:40 +0530

    antd remotion player template

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Player } from "@remotion/player";
+import { MyComposition } from './remotion/Composition';
+import { Layout, Row, Col } from 'antd';
+import { Content, Header } from 'antd/es/layout/layout';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout style={{
+      height: "100%",
+    }}>
+      <Header>Hello How are you</Header>
+      <Content style={{
+        height: "100%",
+      }}>
+        <Row>
+          <Col span={8}>Video Editor</Col>
+          <Col span={12}>
+            <Player
+              component={MyComposition}
+              inputProps={{ text: "World" }}
+              durationInFrames={120}
+              compositionWidth={1920}
+              compositionHeight={1080}
+              fps={30}
+              style={{
+                width: 1280,
+                height: 720,
+              }}
+              controls
+            />
+
+          </Col>
+        </Row>
+
+      </Content>
+    </Layout>
   );
 }
 
